33f2980bef8d0c5cd2e447c0f1ff29807b5ba1d0
johanna sun <sunyx.johanna2016@outlook.com>
2020-07-15 11:32:33 -0400

    finish

--- 15 - 3Sum.py
+++ 15 - 3Sum.py
@@ -9,20 +9,28 @@ class Solution:
     def threeSum(self, nums):
 
         result = []
-        nums = sorted(nums)
+        nums.sort()
 
         for i in range(len(nums)):
+
+            # if we already dealt with current target then continue
+            if i > 0 and nums[i-1] == nums[i]:
+                i += 1
+                continue
+
             target = -nums[i]
             j = i + 1
             k = len(nums) - 1
 
-            while j < len(nums) and k > i and j != k:
+            while j < k:
                 currentSmall = nums[j]
                 currentLarge = nums[k]
-                if currentSmall + currentLarge == target and sorted([nums[i],currentSmall,currentLarge]) not in result:
-                    result.append(sorted([nums[i],currentSmall,currentLarge]))
+                if currentSmall + currentLarge == target:
+                    result.append([nums[i],currentSmall,currentLarge])
                     j += 1
-                    k -= 1
+                    # no need to deal with k cuz the set is different if any one amont it is different
+                    while nums[j] == nums[j-1] and j < k:
+                        j += 1
                 else:
                     if (currentSmall + currentLarge) > target:
                         k -= 1
@@ -34,4 +42,4 @@ class Solution:
 
 
 if __name__ == '__main__':
-    print(Solution().threeSum([0,0,0]))
+    print(Solution().threeSum([1,0,0]))
