7617a8a1d8259ded1d78348c2278ffa3f91c6009
johanna sun <sunyx.johanna2016@outlook.com>
2020-07-15 18:18:47 -0400

    +intuition & finish

--- 24 - Swap Nodes in Pairs.py
+++ 24 - Swap Nodes in Pairs.py
@@ -1,4 +1,7 @@
 
+# intuition
+# the next of the firstnode is the result of the swap of the third&fourth nodes
+# the next of the secondNode is the first one
 
 class ListNode:
     def __init__(self, val=0, next=None):
@@ -9,32 +12,10 @@ class ListNode:
 class Solution:
     def swapPairs(self, head: ListNode) -> ListNode:
 
-        if head is None:
+        if not head or not head.next:
             return head
 
-        result = ListNode(0, head)
-        dummy = ListNode(0, head)
-        first = result.next
-        second = first.next
-
-        while first.next is not None:
-
-            first.next = second.next
-            second.next = first
-            dummy.next = second
-
-            if result.val == 0:
-                result.next = second
-                result.val = 1
-
-            if first.next is None:
-                break
-
-            dummy = first
-            print("dummy:", dummy.val)
-            first = first.next
-            print("first: ", first.val)
-            second = first.next
-            print("second: ", second.val)
-
-        return result.next
+        _next = head.next
+        head.next = self.swapPairs(_next.next)
+        _next.next = head
+        return _next
